903bafe0dd8c67dfd4dcafcb0de3bf98e2d72f23
christopherbrown <christopherbrown@berkeley.edu>
2015-04-11 03:07:56 -0700

    Initial graph drawing

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Waypoint : MonoBehaviour {
+
+    public Vector3 pos;
+    public List<GameObject> adjs;
+    private float rad = 0.25f;
+    void Awake()
+    {
+        pos = transform.localPosition;
+    }
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(pos, rad);
+        foreach(GameObject point in adjs) {
+            Gizmos.DrawLine(pos, point.transform.position);
+        }
+        
+    }
+}
