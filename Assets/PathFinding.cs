4568cb521dc5c042ae320c8df7b3c039310597f6
Ko Costarella <kcostarella@airbears2-10-142-171-55.AirBears2.1918.Berkeley.EDU>
2015-04-11 14:18:06 -0700

    ko stuff

---
+++ Assets/PathFinding.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathFinding : MonoBehaviour {
+	private Vector2 bestCoordinate;
+	private Collider2D hitCollider;
+	private GameObject player;
+	private Algorithms.HitPackage hitPackage;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		player.transform.position = Algorithms.GetClosestVector (player.transform.position).bestCoordinate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			Vector3 ScreenPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			Vector2 mousePosition = new Vector2 (ScreenPosition.x, ScreenPosition.y);
+			hitPackage = Algorithms.GetClosestVector (mousePosition);
+			bestCoordinate = hitPackage.bestCoordinate;
+			hitCollider = hitPackage.collider;
+
+			if (!Mathf.Approximately (bestCoordinate.x, 99999.0f)) {
+				player.transform.position = bestCoordinate;
+				if (hitCollider.gameObject.layer == 9) {
+						player.transform.localScale = new Vector3 (0.25f, 0.25f, 1.0f);
+				} else {
+					player.transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+				}
+			}
+		}
+	}
+
+	float ManhattanDistance(Vector2 v1, Vector2 v2) {
+		return Mathf.Abs(v1.x - v2.x) + Mathf.Abs (v1.y - v2.y);
+	}
+}
---
+++ Assets/Scripts/Algorithms.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Algorithms : MonoBehaviour {
+	static Vector2[] directions;
+	// Use this for initialization
+	void Awake () {
+		directions = new Vector2[8]{Vector2.up, Vector2.right, -Vector2.up, -Vector2.right, Vector2.up + Vector2.right,
+			Vector2.up - Vector2.right, -Vector2.up + Vector2.right, -Vector2.up - Vector2.right};
+	}
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public static HitPackage GetClosestVector(Vector2 click)
+	{
+		HitPackage hitPackage = new HitPackage ();
+		float bestDist = 99999.0f;
+		hitPackage.bestCoordinate = new Vector2 (99999.0f, 99999.0f);
+		Collider2D collider = hitPackage.collider;
+		foreach (Vector2 direction in directions) {
+			int mask = 1 << LayerMask.NameToLayer ("Player");
+			mask = ~mask;
+			RaycastHit2D hit = Physics2D.Raycast(click, direction,Mathf.Infinity,mask);
+			if (hit.collider != null) {
+				if (ManhattanDistance(click, hit.point) < bestDist) {
+					bestDist = ManhattanDistance(click, hit.point);
+					hitPackage.bestCoordinate = hit.point;
+					hitPackage.collider = hit.collider;
+				}
+			} 
+		}
+		return (hitPackage);
+	}
+	public class HitPackage
+	{
+		public Vector2 bestCoordinate{ get; set; }
+		public Collider2D collider{ get; set;}
+	}
+
+	public static float ManhattanDistance(Vector2 v1, Vector2 v2) {
+		return Mathf.Abs(v1.x - v2.x) + Mathf.Abs (v1.y - v2.y);
+	}
+}
