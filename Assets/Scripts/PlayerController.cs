58330878a37d401d6d265340dccb5236545d4d5d
Ko Costarella <kcostarella@koko.local>
2015-05-01 11:50:15 -0700

    saving changes...

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	private Animator anim;
+	private AudioSource stepSound;
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
+		stepSound = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
@@ -19,5 +21,11 @@ public class PlayerController : MonoBehaviour {
 
 	public void setAnimationBoolState(string state, bool val) {
 		anim.SetBool (state, val);
+		if (val) {
+			stepSound.Play ();
+		} else {
+			stepSound.Stop();
+		}
+
 	}
 }
