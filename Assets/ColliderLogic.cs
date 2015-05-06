77929795941b71a5627b9bbdca4072183a17e692
Ko Costarella <kcostarella@koko.local>
2015-05-06 02:24:19 -0700

    Chris did some stuff

---
+++ Assets/ColliderLogic.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderLogic : MonoBehaviour {
+
+	private Animator treeAnim;
+	private PolygonCollider2D collider;
+	// Use this for initialization
+	void Start () {
+		treeAnim = GameObject.FindGameObjectWithTag ("ShyTree").GetComponent<Animator> ();
+		collider = GetComponent<PolygonCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		collider.enabled = !treeAnim.GetBool ("Locked");
+	}
+}
