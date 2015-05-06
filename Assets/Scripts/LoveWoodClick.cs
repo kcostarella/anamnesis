fe6260662f0829d30f5f466b61f1ccb7046269eb
Ko Costarella <kcostarella@koko.local>
2015-05-06 03:09:20 -0700

    All wood in

---
+++ Assets/Scripts/LoveWoodClick.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoveWoodClick : MonoBehaviour {
+	
+	public SpriteRenderer woodSprite;
+	bool woodFadeIn;
+	private float startTime;
+	Animator loveTreeAnim;
+	PlayerStatus playerStatus;
+	PolygonCollider2D polyCol;
+	private bool happened = false;
+
+	// Use this for initialization
+	void Start () {
+		playerStatus = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerStatus> ();
+		polyCol = GetComponent<PolygonCollider2D> ();
+		polyCol.enabled = false;
+		woodSprite = GetComponent<SpriteRenderer> ();
+		woodFadeIn = false;
+		loveTreeAnim = GameObject.FindGameObjectWithTag ("LoveTree").GetComponent<Animator> ();
+	}
+
+	void Update() {
+		if (!happened && loveTreeAnim.GetBool ("Love")) {
+			woodFadeIn = true;
+			startTime = Time.time;
+			polyCol.enabled = true;
+			happened = !happened;
+		}
+
+		if (woodFadeIn) {
+			float t = (Time.time - startTime) / 1f;
+			if(woodSprite != null)
+				woodSprite.color = new Color(1f, 1f, 1f, Mathf.SmoothStep(0f, 1f, t));
+		}
+	}
+	
+	void OnMouseDown() {
+		Destroy(gameObject);
+		playerStatus.numWood += 1;
+	}
+
+}
