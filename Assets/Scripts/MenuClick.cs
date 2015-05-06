3ce14938caa934206dc6224d5e7dc7db37c83916
Chris Brown <christopherbrown@berkeley.edu>
2015-05-06 01:56:04 -0700

    Basic start screen

---
+++ Assets/Scripts/MenuClick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuClick : MonoBehaviour {
+
+	// Use this for initialization
+    void OnMouseDown()
+    {
+        Debug.Log("Loading shit");
+        Application.LoadLevel(1);
+    }
+}
